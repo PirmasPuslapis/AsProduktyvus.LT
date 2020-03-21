a36d041de0dde0f401ae96f7d7ffb9f34bfd7142
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2020-03-21 13:12:33 +0000

    add g analytics

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue';
 import Landing from './views/Landing.vue';
 import Practices from './views/Practices.vue';
 import Team from './views/Team.vue';
+import VueAnalytics from 'vue-analytics'
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-161526517-2'
+})
 
 const routes = {
   '/landing': Landing,
